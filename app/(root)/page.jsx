362fa2a1b9ea562f4417a6eb92eb217e4d993c54
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2025-04-07 11:50:01 +0530

    shown generated interviews on the page

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,12 +1,25 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 import { dummyInterviews } from '@/constants'
+import { getCurrentUser } from '@/lib/actions/auth.action'
+import { getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/genral.action'
 
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Page = () => {
+const Page = async() => {
+
+  const user=await getCurrentUser();
+  const [userInterviews,latestInterviews]= await Promise.all([
+    await getInterviewsByUserId(user?.id),
+    await getLatestInterviews({userId:user?.id})
+  ])
+  
+   
+  const prevInterviews= userInterviews?.length > 0;
+  const hasUpcomingInterviews= latestInterviews?.length > 0;
+ 
 
   return (
     <>
@@ -26,7 +39,11 @@ const Page = () => {
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Your Interviews</h2>
         <div className='interviews-section'>
-          {dummyInterviews.map((interview)=> <InterviewCard data={interview} key={interview.id}/>)}
+          {prevInterviews ? userInterviews.map((interview)=> <InterviewCard data={interview} key={interview.id}/>):
+             <div className='flex justify-center text-lg w-full' >
+             <p> Yoy have'nt taken any interview yet</p>
+           </div>
+          }
 
         </div>
 
@@ -34,8 +51,11 @@ const Page = () => {
       <section className='flex flex-col gap-6 mt-8' >
         <h2>Take an Interview</h2>
         <div className='interviews-section'>
-        {dummyInterviews.map((interview)=> <InterviewCard data={interview} key={interview.id}/>)}
-        {/* <p>You have'nt any ineterview yet.</p> */}
+        {hasUpcomingInterviews ? latestInterviews.map((interview)=> <InterviewCard data={interview} key={interview.id}/>):
+             <div className='flex justify-center text-lg w-full' >
+               <p>---- There are no new interviews available   -----</p>
+             </div>
+          }
         </div>
         
       </section>
